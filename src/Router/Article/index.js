82a2c9e02529b82b3ab8b3834cca172861ea0bf4
Evgenia Maniati <evgenia.maniati@intrasoft-intl.com>
2021-03-01 09:37:59 +0200

    Fix err log on article display view

--- src/Router/Article/index.js
+++ src/Router/Article/index.js
@@ -10,7 +10,7 @@ const ArticleDetails = () => {
         }).then((res) => {
             console.log(res);
         }).catch((err) => {
-            console.log(res);
+            console.log(err);
         })
 
     }, [])
