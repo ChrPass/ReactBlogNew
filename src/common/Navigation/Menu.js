9d2ac5dd3bb215936812949ddeef12d3314da856
PASIOPOULOS Christos <pasiopoulosc@intrasoft-intl.com>
2021-03-07 17:02:22 +0200

    Fixed Navigation Item Avtivation Bug

--- src/common/Navigation/Menu.js
+++ src/common/Navigation/Menu.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
@@ -10,13 +10,16 @@ const useStyles = makeStyles({
   },
 });
 
+const navPaths = ["/Home", "/Beginners", "/WebDev"];
+
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
+  const location = useLocation();
   const [value, setValue] = React.useState(0);
 
   return (
     <BottomNavigation
-      value={value}
+      value={location.pathname === navPaths[value] ? value : null}
       onChange={(event, newValue) => {
         setValue(newValue);
       }}
@@ -26,6 +29,6 @@ export default function SimpleBottomNavigation() {
       <BottomNavigationAction component={Link} to="/Home" label="HOME" />
       <BottomNavigationAction component={Link} to="/Beginners" label="For Beginners" />
       <BottomNavigationAction component={Link} to="/WebDev" label="Web Development" />
-      </BottomNavigation>
+    </BottomNavigation>
   );
 }
