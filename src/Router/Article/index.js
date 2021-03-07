dda0cf792ae02239cd8015dda44ab3a7857bd0a3
ChrPass <xristospas97@gmail.com>
2021-03-07 18:50:20 +0200

    Added title in Article

--- src/Router/Article/index.js
+++ src/Router/Article/index.js
@@ -14,26 +14,32 @@ import gfm from "remark-gfm";
 import { MyImage, CodeBlock } from "../../common/MarkDownRenderer";
 import { getArticles, getArticle } from "../../api/index";
 import RandomPost from "../../components/RandomPost";
+import ArticleTooltip from "../../components/ArticleTooltip";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5),
+    },
+  },
+  articleInfo: {
+    paddingBottom: 2,
+  },
+  breakWord: {
+    overflowWrap: "break-word",
+    wordBreak: "break-word",
+  },
+}));
 
 const ArticleDetails = () => {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      display: "flex",
-      justifyContent: "center",
-      flexWrap: "wrap",
-      "& > *": {
-        margin: theme.spacing(0.5),
-      },
-    },
-  }));
-
   const classes = useStyles();
-
   const { articleId } = useParams();
-  const [article, setArticle] = useState([]);
+  const [article, setArticle] = useState();
   const [allArticles, setAllArticles] = useState([]);
   const [articleTags, setArticleTags] = useState([]);
-  const [articleUser, setArticleUser] = useState([]);
   const setLoader = useContext(LoaderContext);
 
   const fetchArticles = async () => {
@@ -49,7 +55,6 @@ const ArticleDetails = () => {
 
     setArticle(articleRes.details.data);
     setArticleTags(articleRes.details.data.tags);
-    setArticleUser(articleRes.details.data.user.name);
     setAllArticles(allArticlesRes.error ? [] : allArticlesRes.details.data);
     setLoader(false);
   };
@@ -62,53 +67,82 @@ const ArticleDetails = () => {
 
   return (
     <div>
-      <Box
-        style={{
-          height: "500px",
-          width: "100%",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundImage: `url(${article.cover_image})`,
-        }}
-      ></Box>
-      <Container>
-        <Grid container spacing="1">
-          <Grid item xs="8">
-            <Paper>
-              <Box m={6}>
-                <Grid container spacing="2">
-                  <Grid item xs="12" zeroMinWidth>
-                    <div className={classes.root}>
-                      {Object.values(articleTags).map((tag, index) => (
-                        <Chip size="small" label={tag} color="primary" />
-                      ))}
-                    </div>
-                    <Typography
-                      style={{
-                        overflowWrap: "break-word",
-                        wordBreak: "break-word",
-                      }}
-                    >
-                      <ReactMarkdown
-                        source={article.body_markdown}
-                        plugins={[gfm]}
-                        renderers={{ image: MyImage, code: CodeBlock }}
-                        className={{
-                          overflowWrap: "break-word",
-                          wordBreak: "break-word",
-                        }}
-                      />
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Box>
-            </Paper>
-          </Grid>
-          <Grid item xs="4">
-            <RandomPost articles={allArticles} />
-          </Grid>
-        </Grid>
-      </Container>
+      {article && (
+        <>
+          <Box
+            style={{
+              height: "500px",
+              width: "100%",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundImage: `url(${article.cover_image})`,
+            }}
+          ></Box>
+          <Container>
+            <Grid container spacing="1">
+              <Grid item xs="8">
+                <Paper>
+                  <Box m={6}>
+                    <Grid container spacing="2">
+                      <Grid item xs="12" zeroMinWidth>
+                        <Grid
+                          container
+                          direction="column"
+                          justify="center"
+                          alignItems="center"
+                          spacing={2}
+                        >
+                          <Grid item xs>
+                            <Typography
+                              className={classes.breakWord}
+                              variant="h4"
+                            >
+                              <Box fontWeight="fontWeightBold">
+                              {article.title}
+                              </Box>
+                            </Typography>
+                          </Grid>
+
+                          <Grid item xs>
+                            <div className={classes.root}>
+                              {Object.values(articleTags).map((tag, index) => (
+                                <Chip
+                                  size="small"
+                                  label={tag}
+                                  color="primary"
+                                />
+                              ))}
+                            </div>
+                          </Grid>
+                          <Grid item xs>
+                            <ArticleTooltip
+                              userName={article.user.name}
+                              creationDate={article.created_at}
+                              commentsCount={article.comments_count}
+                              reactionsCount={article.public_reactions_count}
+                            />
+                          </Grid>
+                        </Grid>
+                        <Typography className={classes.breakWord}>
+                          <ReactMarkdown
+                            source={article.body_markdown}
+                            plugins={[gfm]}
+                            renderers={{ image: MyImage, code: CodeBlock }}
+                            className={classes.breakWord}
+                          />
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item xs="4">
+                <RandomPost articles={allArticles} />
+              </Grid>
+            </Grid>
+          </Container>
+        </>
+      )}
     </div>
   );
 };
